68b631d4c3032692ae08af38a37bc075fbec40df
Andres Quan <andresquan1@gmail.com>
2017-07-21 23:29:31 -0600

    Give the chef code structure and make it build recipe objects.

--- chef/chef.py
+++ chef/chef.py
@@ -2,8 +2,20 @@
 #Author: Andres Quan
 #Date: 21 July 2017
 
+import Recipe
+import json
+
 def main():
-    pass
+    while True:
+        more = "Would you like to create a new recipe? "
+        if more == "yes":
+            out = Recipe()
+        elif more == "no":
+            print("Have a nice day!")
+            return
+        else:
+            print("I didn't quite understand that. Please say (yes/no)")
+
 
 
 if __name__ == "__main__":
--- chef/recipe.py
+++ chef/recipe.py
@@ -2,11 +2,30 @@
 #Author: Andres Quan
 #Date: 21 July 2017
 
+import step
+import time
+
 class Recipe:
     """A new class to hold a recipe to be built by the chef"""
-    #A dictionary of ingredients and their respective quantities
-    ingredients = {}
+    def __init__(self):
+        this.name = input("What is the title of this Recipe? ")
+        this.author = input("Who is the author? ")
+        
+        this.ingredients = {}
+        while True:
+            more = input("Do you have an ingredient to add?")
+            more = more.lower()
+            if more == "yes":
+                ingredient = input("What is the name of the ingredient? ")
+                quantity = input("How much " + ingredient + " do you use?")
+                this.ingredients[ingredient] = quantity
+            elif more == "no":
+                break
+            else:
+                print("I did not understand please say (yes/no)"
 
-    #Create a new step object TODO not yet defined step object
-    steps = Step()
+
+
+        this.steps = Step()
+        this.date = time.strftime("%d/%m/%y")
 
